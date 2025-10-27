3f4013e12e667b9bda10fbed1982e123a02756ab
IsadoraMarin <icarolinamaring@gmail.com>
2025-10-27 13:27:28 -0300

    Anotaciones

--- app/src/main/java/com/example/appconversacion/ui/Screens/RegistroScreen.kt
+++ app/src/main/java/com/example/appconversacion/ui/Screens/RegistroScreen.kt
@@ -31,7 +31,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBar
 
-
+//Basicamente toda la parte de registro
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RegistroScreen(navController: NavController, viewModel: UsuarioViewModel){
