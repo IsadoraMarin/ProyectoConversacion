8bcbc3d67a9f427ada5f41b2d479e0d4321ecbe7
IsadoraMarin <icarolinamaring@gmail.com>
2025-10-27 13:22:31 -0300

    Anotaciones

--- app/src/main/java/com/example/appconversacion/ui/Screens/GestionarPostScreen.kt
+++ app/src/main/java/com/example/appconversacion/ui/Screens/GestionarPostScreen.kt
@@ -6,7 +6,6 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.isDebugInspectorInfoEnabled
 import androidx.compose.ui.unit.dp
 import com.example.appconversacion.ViewModel.PostViewModel
 import androidx.compose.material.icons.Icons
